9d080d60932950120769690b6899f6a2bae7ef0d
Yassine Moulat El Ali <yassine.moulatelali@usmba.ac.ma>
2024-08-17 20:25:55 +0100

    Modifying Home Page

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -22,6 +22,11 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
